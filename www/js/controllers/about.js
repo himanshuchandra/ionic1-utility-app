0ace6be2b98e80851ae1793cecd512229ea90cba
Himanshu Chandra <himanshu.chandra@outlook.com>
2018-03-30 14:08:13 +0530

    file upload

--- www/js/controllers/about.js
+++ www/js/controllers/about.js
@@ -8,9 +8,9 @@
  * Controller of the appskeleton
  */
 angular.module('appskeleton')
-  .controller('AboutCtrl', function ($scope, $timeout, $cordovaFileTransfer) {
+  .controller('AboutCtrl', function ($scope, $timeout, $cordovaCamera, $cordovaFile, $cordovaFileTransfer, $cordovaDevice, $ionicPopup, $cordovaActionSheet) {
 
-    $scope.uploadPhoto = function(imageURI) {
+    $scope.uploadPhoto = function (imageURI) {
       var options = new FileUploadOptions();
       options.fileKey = "file";
       options.fileName = imageURI.substr(imageURI.lastIndexOf('/') + 1);
@@ -22,25 +22,135 @@ angular.module('appskeleton')
       options.params = params;
       options.chunkedMode = false;
 
-     var ft = new FileTransfer();
-      ft.upload(imageURI, "http://escale-alb-59370729.ap-south-1.elb.amazonaws.com/profile/uploadPic", function(result){
-      console.log(JSON.stringify(result));
-      }, function(error){
-      console.log(JSON.stringify(error));
+      var ft = new FileTransfer();
+      ft.upload(imageURI, "http://escale-alb-59370729.ap-south-1.elb.amazonaws.com/profile/uploadPic", function (result) {
+        console.log(JSON.stringify(result));
+      }, function (error) {
+        console.log(JSON.stringify(error));
       }, options);
     }
 
-    $scope.getImage=function() {
-      navigator.camera.getPicture($scope.uploadPhoto, function(message) {
-      alert('get picture failed');
+    $scope.getImage = function () {
+      navigator.camera.getPicture($scope.uploadPhoto, function (message) {
+        alert('get picture failed');
       }, {
-      quality: 100,
-      destinationType: navigator.camera.DestinationType.FILE_URI,
-      sourceType: navigator.camera.PictureSourceType.PHOTOLIBRARY
-      });
+          quality: 100,
+          destinationType: navigator.camera.DestinationType.FILE_URI,
+          sourceType: navigator.camera.PictureSourceType.PHOTOLIBRARY
+        });
 
     }
 
+    ////////////////////////////
 
+    $scope.showAlert = function (title, msg) {
+      var alertPopup = $ionicPopup.alert({
+        title: title,
+        template: msg
+      });
+    };
+
+    $scope.loadImage = function () {
+      var options = {
+        title: 'Select Image Source',
+        buttonLabels: ['Load from Library', 'Use Camera'],
+        addCancelButtonWithLabel: 'Cancel',
+        androidEnableCancelButton: true,
+      };
+      $cordovaActionSheet.show(options).then(function (btnIndex) {
+        var type = null;
+        if (btnIndex === 1) {
+          type = Camera.PictureSourceType.PHOTOLIBRARY;
+        } else if (btnIndex === 2) {
+          type = Camera.PictureSourceType.CAMERA;
+        }
+        if (type !== null) {
+          $scope.selectPicture(type);
+        }
+      });
+    };
+
+    $scope.selectPicture = function (sourceType) {
+      var options = {
+        quality: 100,
+        destinationType: Camera.DestinationType.FILE_URI,
+        sourceType: sourceType,
+        saveToPhotoAlbum: false
+      };
+
+      $cordovaCamera.getPicture(options).then(function (imagePath) {
+        // Grab the file name of the photo in the temporary directory
+        var currentName = imagePath.replace(/^.*[\\\/]/, '');
+
+        //Create a new name for the photo
+        var d = new Date(),
+          n = d.getTime(),
+          newFileName = n + ".jpg";
+
+        // If you are trying to load image from the gallery on Android we need special treatment!
+        if ($cordovaDevice.getPlatform() == 'Android' && sourceType === Camera.PictureSourceType.PHOTOLIBRARY) {
+          window.FilePath.resolveNativePath(imagePath, function (entry) {
+            window.resolveLocalFileSystemURL(entry, success, fail);
+            function fail(e) {
+              console.error('Error: ', e);
+            }
+
+            function success(fileEntry) {
+              var namePath = fileEntry.nativeURL.substr(0, fileEntry.nativeURL.lastIndexOf('/') + 1);
+              // Only copy because of access rights
+              $cordovaFile.copyFile(namePath, fileEntry.name, cordova.file.dataDirectory, newFileName).then(function (success) {
+                $scope.image = newFileName;
+              }, function (error) {
+                $scope.showAlert('Error', error.exception);
+              });
+            };
+          }
+          );
+        } else {
+          var namePath = imagePath.substr(0, imagePath.lastIndexOf('/') + 1);
+          // Move the file to permanent storage
+          $cordovaFile.moveFile(namePath, currentName, cordova.file.dataDirectory, newFileName).then(function (success) {
+            $scope.image = newFileName;
+          }, function (error) {
+            $scope.showAlert('Error', error.exception);
+          });
+        }
+      },
+        function (err) {
+          // Not always an error, maybe cancel was pressed...
+        })
+    };
+
+    // Returns the local path inside the app for an image
+    $scope.pathForImage = function (image) {
+      if (image === null) {
+        return '';
+      } else {
+        return cordova.file.dataDirectory + image;
+      }
+    };
+
+    $scope.uploadImage = function() {
+      // Destination URL
+      var url = "http://escale-alb-59370729.ap-south-1.elb.amazonaws.com/profile/uploadPic";
+
+      // File for Upload
+      var targetPath = $scope.pathForImage($scope.image);
+
+      // File name only
+      var filename = $scope.image;;
+
+      var options = {
+        fileKey: "file",
+        fileName: filename,
+        chunkedMode: false,
+        mimeType: "multipart/form-data",
+        params : {'fileName': filename}
+      };
+
+      $cordovaFileTransfer.upload(url, targetPath, options).then(function(result) {
+        $scope.showAlert('Success', 'Image upload finished.');
+      });
+    }
 
   });
